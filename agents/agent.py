e7956d3c148caa588d00418c5e7a2437e48c035e
Mali Bhanu Prakash <68366783+Helius-18@users.noreply.github.com>
2025-07-29 18:15:13 +0530

    Update agent.py

--- agents/agent.py
+++ agents/agent.py
@@ -2,13 +2,13 @@ from langchain.agents import initialize_agent, AgentType
 from langchain.memory import ConversationBufferMemory
 from tools.weather_tool import weather_tool
 from tools.general_tool import general_tool
-from tools.dmg_tool import prevalidation_errors_tool, dq_errors_tool
+# from tools.dmg_tool import prevalidation_errors_tool, dq_errors_tool
 from models.llama_model import get_llm
 
 def get_agent():
     llm = get_llm()
     # tools = [weather_tool, general_tool]
-    tools = [weather_tool, prevalidation_errors_tool, dq_errors_tool]
+    tools = [weather_tool]
     
     # Add memory to help with context
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
